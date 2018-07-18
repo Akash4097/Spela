1027d50f75dbe2557f59aea7fbd1fbd40b2fc7f1
Akash Singh <goandsatish@gmail.com>
2018-07-18 11:49:24 +0530

    create initial play layout

--- app/src/main/java/com/example/akash/spela_music_player/Album.java
+++ app/src/main/java/com/example/akash/spela_music_player/Album.java
@@ -3,7 +3,7 @@ package com.example.akash.spela_music_player;
 public class Album {
     private String Title;
     private String Artist;
-
+    private String Thumbnail;
 
     public Album(String title, String artist) {
         Title = title;
@@ -24,5 +24,13 @@ public class Album {
 
     public void setArtist(String artist) {
         Artist = artist;
+    }
+
+    public String getThumbnail() {
+        return Thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        Thumbnail = thumbnail;
     }
 }
--- app/src/main/java/com/example/akash/spela_music_player/MyPlaylistActivity.java
+++ app/src/main/java/com/example/akash/spela_music_player/MyPlaylistActivity.java
@@ -2,12 +2,65 @@ package com.example.akash.spela_music_player;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyPlaylistActivity extends AppCompatActivity {
+
+    private List<Album> albumList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_playlist);
+
+        mRecyclerView = findViewById(R.id.myPlaylist_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(albumList);
+        mRecyclerView.setAdapter(mAdapter);
+        
+        prepareAlbumData();
+    }
+
+    private void prepareAlbumData() {
+        Album album = new Album("AgneePath","Sonu nigam");
+        albumList.add(album);
+
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+        album = new Album("Border","Annu malik");
+        albumList.add(album);
+
+        mAdapter.notifyDataSetChanged();
     }
 }
